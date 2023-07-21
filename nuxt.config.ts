7b6046a2686076ea08c30bb107a566f3c8ec4e68
Hasan Erken <hasan.erken@gmail.com>
2023-07-22 00:04:32 +0300

    eslint configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@bg-dev/nuxt-naiveui',
+  ],
+  naiveui: {
+    colorModePreference: 'light',
+    iconSize: 18,
+    themeConfig: {},
+  },
   nitro: {
-    preset: 'vercel-edge'
-  }
+    preset: 'vercel-edge',
+  },
+
+  build: {
+  },
+
+  vite: {
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     Index Page for nuxt
+    <n-card>
+      <n-button>Merhaba</n-button>
+      Bu bir deneme
+    </n-card>
+    <n-card title="Merhaba" size="medium" class="">ne haber</n-card>
+
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { NCard } from 'naive-ui'
 </script>
 
 <style scoped>
