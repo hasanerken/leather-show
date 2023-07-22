a48c4977b991f4ab0cc517ebdfcec79ab51709c5
Hasan Erken <hasan.erken@gmail.com>
2023-07-22 09:01:32 +0300

    add tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+import type { ThemeConfig } from '@bg-dev/nuxt-naiveui'
+import { theme } from '#tailwind-config'
+
+const themeConfig: ThemeConfig = {
+  shared: {
+    common: {
+      fontFamily: theme.fontFamily.sans.join(', '),
+      lineHeight: theme.lineHeight.normal,
+    },
+  },
+  light: {
+    common: {
+      primaryColor: theme.colors.blue[600],
+      primaryColorHover: theme.colors.blue[500],
+      primaryColorPressed: theme.colors.blue[700],
+    },
+  },
+  dark: {
+    common: {
+      primaryColor: theme.colors.blue[500],
+      primaryColorHover: theme.colors.blue[400],
+      primaryColorPressed: theme.colors.blue[600],
+    },
+  },
+}
+</script>
+
 <template>
   <div>
-   <NuxtPage />
+    <NaiveConfig :theme-config="themeConfig">
+      <n-dialog-provider>
+        <n-message-provider>
+          <NuxtPage />
+        </n-message-provider>
+      </n-dialog-provider>
+    </NaiveConfig>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@bg-dev/nuxt-naiveui',
-  ],
+  modules: ['@bg-dev/nuxt-naiveui', '@nuxtjs/tailwindcss'],
   naiveui: {
     colorModePreference: 'light',
     iconSize: 18,
     themeConfig: {},
+  },
+  tailwindcss: {
+    exposeConfig: true,
   },
   nitro: {
     preset: 'vercel-edge',
