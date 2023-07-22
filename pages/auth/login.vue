0ffc37c619ed11f1077f202108c70e7285186891
Hasan Erken <hasan.erken@gmail.com>
2023-07-22 11:02:15 +0300

    remove supabase auth6

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,84 +1,83 @@
 <script setup lang="ts">
-// import type { FormInst, FormItemRule, FormRules, FormValidationError } from 'naive-ui'
+import type { FormInst, FormItemRule, FormRules, FormValidationError } from 'naive-ui'
 
-// const { auth } = useSupabaseAuthClient()
+const client = useSupabaseAuthClient()
 
-// interface ILogin {
-//   email: string
-//   password: string
-// }
+interface ILogin {
+  email: string
+  password: string
+}
 
-// const formRef = ref<FormInst | null>(null)
-// const message = useMessage()
+const formRef = ref<FormInst | null>(null)
+const message = useMessage()
 
-// const registration = reactive<ILogin>({
-//   email: '',
-//   password: '',
-// })
+const registration = reactive<ILogin>({
+  email: '',
+  password: '',
+})
 
-// const rules: FormRules = {
-//   email: [
-//     {
-//       required: true,
-//       validator(rule: FormItemRule, value: string) {
-//         if (!value)
-//           return new Error('Email is required')
+const rules: FormRules = {
+  email: [
+    {
+      required: true,
+      validator(rule: FormItemRule, value: string) {
+        if (!value)
+          return new Error('Email is required')
 
-//         else if (!/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(value))
-//           return new Error('Not valid email')
+        else if (!/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/.test(value))
+          return new Error('Not valid email')
 
-//         return true
-//       },
-//       trigger: ['input', 'blur'],
-//     },
-//   ],
-//   password: [
-//     {
-//       required: true,
-//       message: 'Password is required',
-//     },
-//   ],
-// }
+        return true
+      },
+      trigger: ['input', 'blur'],
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: 'Password is required',
+    },
+  ],
+}
 
-// const router = useRouter()
-// function handlePasswordInput() {
-// }
-// function handleValidateButtonClick(e: MouseEvent) {
-//   e.preventDefault()
-//   formRef.value?.validate(
-//     (errors: Array<FormValidationError> | undefined) => {
-//       if (!errors) {
-//         signIn()
-//         router.push({ name: 'auth-profile' })
-//       }
-//       else { message.error('Invalid') }
-//     },
-//   )
-// }
-// const errorMsg = ref(null)
+const router = useRouter()
+function handlePasswordInput() {
+}
+function handleValidateButtonClick(e: MouseEvent) {
+  e.preventDefault()
+  formRef.value?.validate(
+    (errors: Array<FormValidationError> | undefined) => {
+      if (!errors) {
+        signIn()
+        router.push({ name: 'auth-profile' })
+      }
+      else { message.error('Invalid') }
+    },
+  )
+}
+const errorMsg = ref(null)
 
-// async function signIn() {
-//   try {
-//     const { error } = await auth.signInWithPassword({
-//       email: registration.email,
-//       password: registration.password,
-//     })
+async function signIn() {
+  try {
+    const { error } = await client.auth.signInWithPassword({
+      email: registration.email,
+      password: registration.password,
+    })
 
-//     if (error) {
-//       message.error(error.message)
-//       throw error
-//     }
-//   }
-//   catch (error: any) {
-//     errorMsg.value = error.message
-//   }
-// }
+    if (error) {
+      message.error(error.message)
+      throw error
+    }
+  }
+  catch (error: any) {
+    errorMsg.value = error.message
+  }
+}
 </script>
 
 <template>
   <div class="flex justify-center">
-    Hi world,
-    <!-- <n-card class="m-10 p-5 bg-blue-50" style="max-width: 500px;">
+    <n-card class="m-10 p-5 bg-blue-50" style="max-width: 500px;">
       <n-form ref="formRef" :model="registration" :rules="rules" class="">
         <n-form-item path="email" label="e-Mail">
           <n-input v-model:value="registration.email" @keydown.enter.prevent />
@@ -108,7 +107,7 @@
     </n-card>
     <div>
       {{ errorMsg }}
-    </div> -->
+    </div>
   </div>
 </template>
 
