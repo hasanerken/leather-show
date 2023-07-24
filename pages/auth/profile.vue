a6c180462a3b7441cfc0a5e447cc197dbd2b89ef
Hasan Erken <hasan.erken@gmail.com>
2023-07-24 18:01:23 +0300

    tenant url corrected

--- pages/auth/profile.vue
+++ pages/auth/profile.vue
@@ -5,7 +5,7 @@ const supabase = useSupabaseClient<Database>()
 const message = useMessage()
 const { data, error } = await supabase.from('tenants').select('*') // upsert({ email: 'mybest@gmail.com' })
 
-const mydata = await $fetch('http://leather-show.railway.internal/tenants')// Use $fetch with your api routes to get typesafety
+const mydata = await $fetch('https://xfiber-production.up.railway.app/tenants')// Use $fetch with your api routes to get typesafety
 if (error)
   message.warning('Communication can not taken')
 </script>
