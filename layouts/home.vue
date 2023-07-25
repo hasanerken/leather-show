f8244b7a795a778643a26d7550b44c1a73415bd1
Hasan Erken <hasan.erken@gmail.com>
2023-07-25 18:50:42 +0300

    add logo psh

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,13 +1,18 @@
-<script>
-export default {
-
-}
+<script setup lang="ts">
+import PshLogo from '~/assets/svgs/psh-logo.svg'
 </script>
 
 <template>
   <div class="">
-    <header class="h-16">
-      HEADER PART
+    <header>
+      <n-space align="center" class="h-20 via-transparent">
+        <div class="pl-3">
+          <PshLogo style="height: 40px;" :font-controlled="false" class="" />
+        </div>
+        <p class="text-2xl text-center text-red-600 font-thin tracking-wider">
+          PrepShipHub
+        </p>
+      </n-space>
     </header>
     <slot />
   </div>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -12,14 +12,14 @@ definePageMeta({
       <n-space justify="center" align="center" wrap class="mt-24">
         <n-card :bordered="false" style="max-width: 800px;">
           <h1 class="mb-6 text-3xl font-bold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-            <span class="text-transparent bg-clip-text bg-gradient-to-r to-purple-500 from-lime-400">First Class Solutions</span> for Streamlined e-commerce Excellence
+            <span class="text-transparent bg-clip-text bg-gradient-to-r from-red-600  to-lime-400">First Class Solutions</span> for Streamlined e-commerce Excellence
           </h1>
           <p class="font-light text-lg text-gray-700 lg:text-2xl dark:text-gray-400 spacey">
             Unlock seamless operations and efficiency with PrepShipHub - your trusted partner for faster, more reliable, and cost-effective e-commerce solutions.
             Join forces with e-commerce sellers and merchants to create an unbeatable customer experience!
           </p>
           <n-space class="mt-8">
-            <n-button color="#7A3D8D" class="h-10 w-40 p-6">
+            <n-button color="#EE2B3A" class="h-10 w-40 p-6">
               More Information
             </n-button>
             <n-button color="#B3D764" class="h-10 w-40 p-6">
