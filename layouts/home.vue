8619bcb4804e197f8c4a8cdffe6d44809bf0ede7
Hasan Erken <hasan.erken@gmail.com>
2023-07-25 20:52:45 +0300

    try for performance without naive

--- layouts/home.vue
+++ layouts/home.vue
@@ -5,13 +5,16 @@ import PshLogo from '~/assets/svgs/psh-logo.svg'
 <template>
   <div class="">
     <header>
-      <n-space align="center" class="h-20 via-transparent">
-        <div class="pl-3">
-          <PshLogo style="height: 40px;" :font-controlled="false" class="" />
+      <n-space justify="space-between" align="center" class="h-20 bg-gradient-to-r" style="background-image: linear-gradient(to bottom, rgba(197, 127, 127, 0.3), rgba(247, 247, 247, 0.1));">
+        <n-space align="center" class="pl-3">
+          <PshLogo style="height: 40px;" :font-controlled="false" />
+          <p class="text-2xl text-center text-red-600 font-thin tracking-wider">
+            PrepShipHub
+          </p>
+        </n-space>
+        <div class="">
+          d
         </div>
-        <p class="text-2xl text-center text-red-600 font-thin tracking-wider">
-          PrepShipHub
-        </p>
       </n-space>
     </header>
     <slot />
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -7,32 +7,33 @@ definePageMeta({
 </script>
 
 <template>
-  <n-layout>
-    <n-layout-content>
-      <n-space justify="center" align="center" wrap class="mt-24">
-        <n-card :bordered="false" style="max-width: 800px;">
+  <div>
+    <div>
+      <div justify="center" align="center" wrap class="mt-24">
+        <div :bordered="false" style="max-width: 800px;">
           <h1 class="mb-6 text-3xl font-bold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             <span class="text-transparent bg-clip-text bg-gradient-to-r from-red-600  to-lime-400">First Class Solutions</span> for Streamlined e-commerce Excellence
           </h1>
           <p class="font-light text-lg text-gray-700 lg:text-2xl dark:text-gray-400 spacey">
-            Unlock seamless operations and efficiency with PrepShipHub - your trusted partner for faster, more reliable, and cost-effective e-commerce solutions.
+            Unlock seamless operations and efficiency with <span class="text-red-600 font-extrabold  px-2 py-1">PrepShipHub</span>
+            &mdash;your trusted partner for faster, more reliable, and cost-effective e-commerce solutions.
             Join forces with e-commerce sellers and merchants to create an unbeatable customer experience!
           </p>
-          <n-space class="mt-8">
-            <n-button color="#EE2B3A" class="h-10 w-40 p-6">
+          <div class="mt-8">
+            <button color="#EE2B3A" class="h-10 w-40 p-6">
               More Information
-            </n-button>
-            <n-button color="#B3D764" class="h-10 w-40 p-6">
+            </button>
+            <button color="#89C225" class="h-10 w-40 p-6">
               Get Started
-            </n-button>
-          </n-space>
-        </n-card>
-        <n-card :bordered="false">
+            </button>
+          </div>
+        </div>
+        <div class="hidden sm:block" :bordered="false">
           <HeroIcons style="width: 480px;" :font-controlled="false" class="" />
-        </n-card>
-      </n-space>
-    </n-layout-content>
-  </n-layout>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
